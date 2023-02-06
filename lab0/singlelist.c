8501f8de04f64d31da260393634d9200e7ccc057
Danan623 <Andersson920131@outlook.com>
2023-02-06 09:53:24 +0100

    upd

--- lab0/singlelist.c
+++ lab0/singlelist.c
@@ -2,57 +2,109 @@
 #include <stdlib.h>
 #include "singlelist.h"
 
-//put x at the end of the list
-void append(struct list_item *first, int x){
-    struct list_item *link = (struct list_item*) malloc(sizeof(struct list_item)); //allocate memory for pointer between structs
-    struct list_item *ptr = first; //ptr to root item
+    /*
+    puts x at the end of the list
+    */
+    void append(struct list_item *first, int x){
+        // allokera minne : skapa en instans av structen och använd malloc för att allokera mer minne vid varje insättning, strl = strl av structen efter ny insättning
+        // skapa en instans av root minnet för att veta vart "HEAD" är dvs position 0
+        // loopa från position 0 till "struct.length", sätt därefter in det nya element på den tomma positionen (pointer == null)
+        // return
 
-    while(ptr->next != NULL) { //while there are still items to iterate over
-       ptr = ptr->next;
+        // no need to continue if the list is empty
+        if(first == NULL){
+            return;
+        } 
+        // create an instance of the struct "list_item", use malloc to allocate more memory for every append. size = size of the struct.
+        struct list_item *new_node = (struct list_item*)malloc(sizeof(struct list_item));
+        // create an instance ... , first is the root node (pos 0 in the list). this will give us the entry to the list in memory. 
+        struct list_item *parent_node = first; // root node
+        // assign the argument x to value and set the next node to empty (null) in the list
+        new_node->value = x;
+        new_node->next = NULL;
+
+        // iterate from position 0 to end of list
+        while(parent_node->next != NULL){    
+            parent_node = parent_node->next;  
+        }
+        // assign next node = new node (value = x , next = null)
+        parent_node->next = new_node;
+        
+        return; // it's a void so we return nothing
+
     }
-
-    ptr->next = link; //Set link-address for previous node to new node.
-    link->value = x; //Set value in new node to x.
-    link->next = NULL; //Set link-adress for new node to null.
-};
-//put x at the beginning of the list
-void prepend(struct list_item *first, int x){
-    struct list_item *link = (struct list_item*) malloc(sizeof(struct list_item)); //Create new node
-    link->value = x; //Assign value to node
-
-    struct list_item *ptr = first; //Set pointer to root nodes adress.
-    link->next = first->next; //Set what new node points to the same as what root points to.
-    //first->next = link; //Make root point to new node.
-    ptr->next = link; //Make root point to new node.
-};
-//prints all elements in the list
-void print (struct list_item *first){
-    struct list_item *temp = first; //set pointer to root
-
-    while(temp != NULL) { //while we have items to iterate over
-        printf("%d - ", temp->value); //write value of node
-        temp = temp->next; // go to next node
+    /*
+    puts x at the beginning of the list
+    */
+    void prepend(struct list_item *first, int x){
+    // allocate memory for one more node
+    struct list_item *new_child = (struct list_item*)malloc(sizeof(struct list_item));
+    // new_child value
+    new_child->value = x;
+    // assign the root node's ->next node to the new_child node 
+    new_child->next = first->next;
+    // assign new_child node to the old node's -> next node 
+    first->next = new_child;
+    return;
     }
-    printf("\n");
-    free(temp); //deallocate memory for the node
-
-};
-//find the first element in the list larger than x and input x right before that element
-void input_sorted(struct list_item *first, int x) {
-    struct list_item *ptr = first; //set pointer to root
-    while (x > ptr->next->value) { //always ignore root so ptr->next and this nodes value so ->value
-        ptr = ptr->next; //go to next node
+    /*
+    prints all elements in the list
+    */
+    void print(struct list_item *first){
+        // skapa en instans till listan
+        // iterera från pos 0 till end
+        // printa ut varje pos
+        // return
+        struct list_item *root_node = first->next;
+        printf("%s","[");
+        while(root_node != NULL){
+            printf(" %d",root_node->value);
+            root_node = root_node->next;
+        }
+        printf("%s"," ]");
+        
+        return;
     }
-    prepend(ptr, x); //reuse prepend
-};
-//free everything dynamically allocated
-void clear (struct list_item *first){
-    struct list_item *ptr = first->next; //ignore root
-    struct list_item *prev; //ptr to save previous address
-    first->next = NULL; //clear first node after root
-    while (ptr != NULL) { //while we still have items to iterate over
-        prev = ptr; //save address
-        ptr = ptr->next; //go to next node
-        free(prev); //deallocate memory
-    } 
-};
+    
+    /*
+    input sorted: find the first element in the list larger than x 
+    and input x right before that element
+    */
+    void input_sorted(struct list_item *first, int x){
+        // not sure if we want to check this, the first should be assigned.. it's static
+        if(first == NULL){
+            return;
+        }
+        struct list_item *parent_node = first; 
+        // we will iterate and check one step ahead before moving to next node
+        // that will give us the advantage of prepend
+        while((parent_node->next) != NULL && (parent_node->next)->value < x){
+            parent_node = parent_node->next;
+        }
+        // if next mem address is empty or element is bigger then we should assign the new node there 
+        prepend(parent_node,x);
+        return;
+    }
+    /*
+    free everything dynamically allocated
+    */
+    void clear(struct list_item *first){
+        // create instance variable and assign position 1 in the list to it
+        struct list_item *parent_node = first->next;
+        // create a tmp instance variable
+        struct list_item *tmp_node;
+        // iterate from 1 to end of list
+        while(parent_node != NULL){
+            // save next mem location of the parent node
+            tmp_node = parent_node->next;
+            // free all the dynamically allocated mem of the list
+            free(parent_node);
+            // assign next mem location to the parent node
+            parent_node = tmp_node;
+        }
+        // "free" the allocated memory of position 1
+        first->next = NULL;
+        // note : the root node (position 0) is a static allocated node and should not be freed
+
+        return;
+}
--- lab0/singlemain.c
+++ lab0/singlemain.c
@@ -3,20 +3,29 @@
 #include <stdlib.h>
 
 int main() {
-    struct list_item root;
-    root.value = -1;
-    root.next = NULL;
-    append(&root, 5);
-    append(&root, 7);
-    prepend(&root, 3);
-    input_sorted(&root, 6);
-    input_sorted(&root, 2);
-    print(&root);
-    clear(&root);
-    print(&root);
+  struct list_item root;
+    root.value = -1; // set first node (root node) to value -1
+    root.next = NULL; // set next node to null
+
+    append(&root, 10);
+    append(&root, 9);
     append(&root, 8);
-    clear(&root);
-  //  append(&root, 3);
+    append(&root, 7);
+    append(&root, 6);
+    append(&root, -5);
+
     print(&root);
+
+    prepend(&root,-4);
+    print(&root);
+
+    input_sorted(&root,-3);
+    print(&root);
+
+   clear(&root);
+    append(&root,10);
+    print(&root);
+
+    
     return 0;
 };
